3dd69d01c1c227289bf98312fffeb0ecdd824b29
Zarina <gizueva.zarina@gmail.com>
2024-11-07 19:46:26 +0500

    method is_element_present added

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.firefox.options import Options as OptionsFirefox
 import time
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='en', help="Choose language: ru, en, fr etc.")
 
 @pytest.fixture(scope="function")
