a45f135d185486f048146ce9c2e18f2a1eb3e0c8
Zarina <gizueva.zarina@gmail.com>
2024-11-08 19:18:54 +0500

    tests for login page added

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,3 +14,28 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.should_be_login_url()
+
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, link)
+    page.should_be_login_form()
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, link)
+    page.should_be_register_form()
