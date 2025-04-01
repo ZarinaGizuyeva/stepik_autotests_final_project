92bb7f5b91971a363ad528109457ef907a5a9aa9
Zarina <gizueva.zarina@gmail.com>
2025-04-01 21:24:33 +0500

    deleted unused tests from main page tests

--- test_main_page.py
+++ test_main_page.py
@@ -5,49 +5,23 @@ from .pages.basket_page import BasketPage
 
 @pytest.mark.login_guest
 class TestMainPage:
+    def setup_method(self):
+        self.main_page = MainPage(self.browser)
+
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
+        self.main_page.open()
+        self.main_page.go_to_login_page()
+        login_page = LoginPage(self.browser)
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
-        page.open()
-        page.should_be_login_link()
+        self.main_page.open()
+        self.main_page.should_be_login_link()
 
-def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        self.main_page.open()
+        self.main_page.go_to_basket_page()
+        basket_page = BasketPage(self.browser)
+        basket_page.should_be_empty_basket()
+        basket_page.should_be_message_that_basket_empty()
 
-def test_should_be_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_form()
-
-def test_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_register_form()
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_empty_basket()
-    basket_page.should_be_message_that_basket_empty()
