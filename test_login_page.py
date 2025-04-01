beb0d25e058efa5defb9b4021fefed5d5ebe08d2
Zarina <gizueva.zarina@gmail.com>
2025-04-01 21:29:31 +0500

    tests for login page added

---
+++ test_login_page.py
@@ -0,0 +1,22 @@
+import pytest
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+class TestLoginPage:
+    def setup_method(self):
+        self.login_page = LoginPage(self.browser)
+
+    def test_should_be_login_url(self, browser):
+        self.login_page.open()
+        self.login_page.go_to_login_page()
+        self.login_page.should_be_login_url()
+
+    def test_should_be_login_form(self, browser):
+        self.login_page.open()
+        self.login_page.go_to_login_page()
+        self.login_page.should_be_login_form()
+
+    def test_should_be_register_form(self, browser):
+        self.login_page.open()
+        self.login_page.go_to_login_page()
+        self.login_page.should_be_register_form()
